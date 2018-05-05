5d6cf260cdd83e3e4d44f94ad4f770bb5752e46c
Jonathan Kempf <kempfjj@gmail.com>
2018-05-05 18:50:42 -0400

    Replaced rules with specific rules per product type, logging initial event

--- lambda/main.js
+++ lambda/main.js
@@ -3,13 +3,17 @@ const im = require('imagemagick');
 const S3 = new AWS.S3({
 	signatureVersion: 'v4',
 });
-const resizingRules = require('../data/rules.json');
+const cmsimage = require('../data/cmsimage_rules.json');
+const products = require('../data/products_rules.json');
+const stories = require('../data/stories_rules.json');
+const taxons = require('../data/taxons_rules.json');
 const BUCKET = process.env.BUCKET;
 
 // take the image input URL from s3, along with certain rules from the path of the s3 location to determine the correct type of resizing and optimization
 // all product and story images come from the /media/ path.
 const route = (event, context, callback) => {
-	
+    console.log(event);
+    callback(null, 'OK');
 }
 
 const getImage = (imagePath, callback) => {
