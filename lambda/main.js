004b393732a4a01e2d10a7860523cb7e39867b85
Jonathan Kempf <jon@maisonette.com>
2018-05-07 16:10:43 -0400

    Starting to work on the actual data flow to the imageMagick worker. Have commented out the streaming protobuf in favor of passing around a single buffer.
    NEW PACKAGE imagemagick -> gm
    
    Imagemagick node package was deprecated in favor of gm, so converted over to use that.

--- lambda/main.js
+++ lambda/main.js
@@ -1,12 +1,9 @@
+const stream = require('stream')
 const AWS = require('aws-sdk');
-const im = require('imagemagick');
-const S3 = new AWS.S3({
+const gm = require('gm').subClass({imageMagick: true});
+const s3 = new AWS.S3({
   signatureVersion: 'v4',
 });
-const cmsimage = require('../data/cmsimage_rules.json');
-const products = require('../data/products_rules.json');
-const stories = require('../data/stories_rules.json');
-const taxons = require('../data/taxons_rules.json');
 const BUCKET = process.env.BUCKET;
 
 // TO DO: Refactor route into index that calls functional composition: route(getImage(imageWork(setImage()))), to be more testable
@@ -15,23 +12,91 @@ const BUCKET = process.env.BUCKET;
 // all product and story images come from the /media/ path.
 exports.route = (event, context, callback) => {
   console.log(JSON.stringify(event));
-  return getImage(`${event.Records.s3.object.key}`, callback);
+  if (!event.hasOwnProperty('Records') && !event.hasOwnProperty('pathParameters')) {
+    return callback('not a valid request');
+  } 
+  if (event.hasOwnProperty('Records') && event.Records[0].s3.object.key.split('/').includes('original')) {
+    return getImage(`${event.Records[0].s3.object.key}`, callback);
+  } else if (event.hasOwnProperty('Records')) {
+    return callback(null, 'not original image');
+  } else if (event.hasOwnProperty('pathParameters')) {
+    // WIP for API Gateway
+  }
+  return callback('unhandled event condition');
 };
 
-const getImage = (imagePath, callback) => {
+const getImage = async (imagePath, callback) => {
     // get image from s3 via pipe in a promise
   const params = {
     Bucket: BUCKET,
-    Key: imagePath
+    Key: imagePath // 'media/<type>/<folder>/<imageName>.<imageExt>'
   };
 
-  const imageObj = S3.getObject(params).promise();
-  console.log(imageObj);
+  const response = await s3.getObject(params, (err) => {
+    if (err) {
+      callback(err);
+    }
+  }).promise();
+
+  const imageRulesArguments = {
+    contentType: response.ContentType,
+    data: response.Body,
+    imagePath,
+  };
+
+  return setImageRules(imageRulesArguments, callback);
 };
 
-const imageWork = (args, callback) => {
-  // args will be an object with the data type of image, the Buffer with the image data, the original image path
+const setImageRules = (args, callback) => {
+  const ruleType = args.imagePath.split('/')[1] === ('cmsimage'||'products'||'stories'||'taxons') ? args.imagePath.split('/')[1] : null;
+  if ( ruleType === null ) {
+    return callback('unable to find rule matching image category');
+  }
+  const rules = require(`../data/${ruleType}_rules.json`);
+  console.log(rules);
+  return imageWork(rules, {contentType: args.contentType, imageData: args.data}, callback)
+}
 
+const imageWork = (rules, args, callback) => {
+  // args will be an object with the data type of image, the Buffer with the image data, the original image path
+  // const pipeline = new stream.PassThrough();
+  // pipeline.setMaxListeners(0);
+  // pipeline.end(args.imageData);
+  Object.entries(rules).forEach(([imageKey, imageVal]) => {
+    // console.log(pipeline._readableState.pipes !== null ? pipeline._readableState.pipes.getMaxListeners() : "Hi");
+    const magickArgs = [
+      args.imageData,
+      '-filter',
+      'Triangle',
+      '-define',
+      'filter:support=2',
+      '-thumbnail',
+      imageVal.width,
+      '-unsharp 0.25x0.25+8+0.065',
+      '-dither None',
+      '-posterize 136',
+      '-quality 82',
+      '-define jpeg:fancy-upsampling=off',
+      '-define png:compression-filter=5',
+      '-define png:compression-level=9',
+      '-define png:compression-strategy=1',
+      '-define png:exclude-chunk=all',
+      '-interlace none',
+      '-colorspace sRGB',
+      '-strip',
+      '-'
+    ];
+    // GM CONVERT CURRENTLY DOES NOT WORK. NEED TO RESEARCH WHY
+    // pipeline.pipe(process.stdout)
+    // gm.convert(magickArgs, (err, stdout, stderr) => {
+    //   if (err) {
+    //     console.log(err)
+    //     callback(err)
+    //   }
+    //   console.log(stdout);
+    //   console.log(stderr);
+    // });
+  })
 };
 
 const setImage = (args, callback) => {
