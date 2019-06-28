3d269a1066eec9406865af1eb2d93a294a1d09fa
Jonathan Kempf <kempfjj@gmail.com>
2019-06-28 11:41:47 -0400

    What are code even?

--- lambda/publishMessage.js
+++ lambda/publishMessage.js
@@ -1,13 +1,17 @@
 const AWS = require('aws-sdk');
+const { TOPIC_ARN, REGION } = process.env;
+AWS.config.update({ region: REGION });
 const sns = new AWS.SNS({
   apiVersion: '2010-03-31'
 });
-const { TOPIC_ARN } = process.env;
+
 
 exports.pub = async (message, response) => {
-  const decoratedMessage = Object.assign({}, message, { response });
+  const messageObject = JSON.parse(message);
+  const decoratedMessage = Object.assign({}, messageObject, { response });
+  console.log('decoratedMessage', decoratedMessage);
   const params = {
-    Message: decoratedMessage,
+    Message: JSON.stringify(decoratedMessage),
     TopicArn: TOPIC_ARN
   };
 
