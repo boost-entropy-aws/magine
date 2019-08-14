e9bd7c52ea72d9242896937b1454e155d15e77fe
Jonathan Kempf <kempfjj@gmail.com>
2019-08-14 14:15:09 -0400

    Bugfix - body.params error
    This is most likely the fix for the intermittent errors we have seen in Magine. When the JSON files were getting uploaded to S3, it was triggering the lambda as well, and then trying to perform imagemagick stuff on it, resulting in failures that were only caught when it tried to re-upload the files to the new S3 location. This is a dirty fix, but it allows for the execution to halt ASAP, as opposed to handling it further down the line when it has already loaded in the imagemagick libs and started to allocate heap resources.

--- lambda/main.js
+++ lambda/main.js
@@ -11,6 +11,12 @@ exports.route = async (event, context, callback) => {
     gmOpts = Object.assign({}, gmOpts, { appPath: `${path.resolve(__dirname, 'bin', 'exodus', 'bin', 'magick')}`, imageMagick: true });
     environment = 'api';
   } else if (event.Records) {
+    const fullLocation = event.Records[0].s3.object.key;
+    const fileName = fullLocation.split('/').pop();
+    const extension = path.ext(fileName);
+    if (extension === '.json') {
+      return callback("Don't work on JSON files");
+    }
     gmOpts = Object.assign({}, gmOpts, { appPath: `${path.resolve(__dirname, 'bin', 'exodus', 'bin', 'magick')}`, imageMagick: true });
     environment = 's3';
   } else if (event.key) {
