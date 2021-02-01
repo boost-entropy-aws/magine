82ed81d102b9de7a84ff0b81f0065360243ed6b4
Dan Fyfe <fyfedaniel@gmail.com>
2021-02-01 17:30:24 -0500

    capitalize plane?

--- lambda/resize.js
+++ lambda/resize.js
@@ -37,7 +37,7 @@ exports.default = async (rules, imageVehicle, storageKey, uuid, imageName, tempO
       '-define',
       'png:exclude-chunk=all',
       '-interlace',
-      'plane',
+      'Plane',
       '-colorspace',
       'sRGB',
       '-strip'
