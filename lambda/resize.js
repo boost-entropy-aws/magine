e359028288db77c683a6bd1a2ba0385df9a869bc
Dan Fyfe <fyfedaniel@gmail.com>
2021-02-02 16:17:51 -0500

    interlace line

--- lambda/resize.js
+++ lambda/resize.js
@@ -12,8 +12,35 @@ exports.default = async (rules, imageVehicle, storageKey, uuid, imageName, tempO
   let returnedImage;
   const resizedImages = Object.entries(rules).map(async ([imageMod, imageDim]) => {
     const magickArgs = [
+      '-filter',
+      'Triangle',
+      '-define',
+      'filter:support=2',
+      '-thumbnail',
+      `${imageDim.width}`,
+      '-unsharp',
+      '0.25x0.25+8+0.065',
+      '-dither',
+      'None',
+      '-posterize',
+      '136',
+      '-quality',
+      '82',
+      '-define',
+      'jpeg:fancy-upsampling=off',
+      '-define',
+      'png:compression-filter=5',
+      '-define',
+      'png:compression-level=9',
+      '-define',
+      'png:compression-strategy=1',
+      '-define',
+      'png:exclude-chunk=all',
       '-interlace',
-      'plane'
+      'line',
+      '-colorspace',
+      'sRGB',
+      '-strip'
     ];
     const magickGifArgs = [
       '+dither',
